da0fb694f3da3e913da8fa41d7510fe9343f54c5
Alexandre <alexandre.jallon@epitech.eu>
2018-06-23 16:03:44 +0200

    Maj services

--- sources/services/Location.js
+++ sources/services/Location.js
@@ -6,14 +6,12 @@ export default class LocationApi {
 
   static sendLocation(coords) {
     //console.log("Location service", coords.latitude, " && ", coords.longitude);
-    PromiseApi.auth().post('/profiles/geo', { lat : coords.latitude, lng : coords.longitude })
+    PromiseApi.post('/profiles/geo', { lat : coords.latitude, lng : coords.longitude })
     .then((result) => {
-      console.log("###################### Location service process : ########################");
       console.log(result);
 
         if (result.error) {
           console.log("Location service error");
-
           LocationActions.sendLocationError(result);
           return;
         }
@@ -31,68 +29,18 @@ export default class LocationApi {
     });
   }
 
-  static getNearGuides(range) {
-    //console.log("getNearGuides service");
-    PromiseApi.auth().get('/profiles/geo/' + 100000)
+  static nearPhotos(range) {
+    PromiseApi.get('/proposals/geo/')
     .then((result) => {
-
-      //console.log("Location service process : ", result);
-
         if (result.error) {
-          console.log("getNearGuides service error");
-          LocationActions.getNearGuidesError(result);
+          console.log("getNearPhotos service error");
+          LocationActions.getNearPhotosError(result);
           return;
         }
-        let ids = [];
-        result.map((guide) => {
-          ids.push(guide._id)
-        });
-        AvatarApi.getAvatars(ids, [])
-          .then(avatars => {
-            // console.log("~~~~~~~~~~~ avatars preview ~~~~~~~~~~~~~~~")
-            // console.log(avatars);
-            // console.log("~~~~~~~~~~~ 0 ~~~~~~~~~~~~~~~")
-            // console.log(avatars[0]);
-            // console.log("~~~~~~~~~~~ 1 ~~~~~~~~~~~~~~~")
-            // console.log(avatars[1]);
-            result.map((guide, idx) => {
-              guide.photoUrl = avatars[idx];
-            });
-            // console.log("~~~~~~~~~~~ avatars ~~~~~~~~~~~~~~~")
-            // console.log(result)
-            LocationActions.getNearGuidesSuccess(result);
-          });
-
-          //   CommentAvatarsActions.getSuccess.defer(
-          //   ids.map((id, index) => {
-          //     return { id, avatar: avatars[index] };
-          //   })
-          // ))
-          // //.catch(err => CommentAvatarsActions.error(err));
+        LocationActions.getNearPhotosSuccess(result);
     })
     .catch((err) => {
-      // console.log("getNearGuides catch ", err);
-        LocationActions.getNearGuidesError(err);
-    });
-  }
-
-  static nearAds(range) {
-
-    PromiseApi.auth().get('/proposals/geo/' + 100000)
-    .then((result) => {
-      //console.log("Location adsssssssssss service process : ", result);
-
-        if (result.error) {
-          console.log("getNearAds service error");
-
-          LocationActions.getNearAdsError(result);
-          return;
-        }
-
-        LocationActions.getNearAdsSuccess(result);
-    })
-    .catch((err) => {
-      console.log("getNearAds catch ", err);
+      console.log("getNearPhotos catch ", err);
         LocationActions.getNearAdsError(err);
     });
   }
--- sources/services/Picture.js
+++ sources/services/Picture.js
@@ -5,39 +5,31 @@ import SearchActions  from 'actions/Search.js';
 export default class PictureApi {
 
   static getMyPictures(id = "1") {
-    //console.log("get Picture service");
     PromiseApi.get("/api/users/" + id + "/photos")
-    // PromiseApi.auth().get('/proposals')
     .then((result) => {
 
         if (result.error) {
-          //console.log("get Picture service error");
           PictureActions.getMyPicturesError(result);
           return;
         }
         PictureActions.getMyPicturesSuccess(result);
     })
     .catch((err) => {
-      //console.log("get Picture service catch ", err);
         PictureActions.getMyPicturesError(err);
     });
   }
 
   static getFriendsPictures(id) {
-    //console.log("get guides Picture service");
     PromiseApi.get('/public/users/' + id + '/proposals')
     .then((result) => {
-      //console.log("requestVisit service process");
 
         if (result.error) {
-          //console.log("get guides Picture error");
           PictureActions.getGuidePicturesError(result);
           return;
         }
         PictureActions.getGuidePicturesSuccess(result);
     })
     .catch((err) => {
-      //console.log("get guides Picture catch ", err);
         PictureActions.getGuidePicturesError(err);
     });
   }
@@ -46,7 +38,6 @@ export default class PictureApi {
     console.log("findPictures");
     PromiseApi.get(`/public/proposals/${PictureId}/imageHooks`)
       .then((hooks) => {
-        //console.log("Hooks", hooks);
         Promise.all(hooks.map(hook => PromiseApi.download(`/public/proposals/${PictureId}/image/${hook}`)))
           .then((photos) => {
             photos.map((photo, idx) => {
@@ -57,16 +48,12 @@ export default class PictureApi {
             PictureActions.PicturePictureSuccess([PictureId, photos]);
           });
       })
-      // .catch((err) => {
-      //   PictureActions.PicturePictureError(err);
-      // });
   }
 
   static findPictures(PictureId) {
     console.log("findPictures");
     PromiseApi.get(`/public/proposals/${PictureId}/imageHooks`)
       .then((hooks) => {
-        //console.log("-----------Hooks-------------", hooks);
         Promise.all(hooks.map(hook => PromiseApi.download(`/public/proposals/${PictureId}/image/${hook}`)))
           .then((photos) => {
             photos.map((photo, idx) => {
@@ -76,19 +63,14 @@ export default class PictureApi {
           });
       })
        .catch((err) => {
-         //PictureActions.PicturePictureError(err);
+        console.log(err);
      });
   }
 
   static createPicture(form) {
     console.log("Picture service !!!", form);
-    const files = form.photos;
-    delete form.photos;
 
-      //const fileUpload = new FileReader();
-      //var name = file.path.split("/");
-      //fileUpload.append('avatar', {uri: file.path, name : name[name.length - 1], type : file.mime});
-    PromiseApi.auth().uploads('/proposals', form, files)
+    PromiseApi.uploads('/api/photos', form)
     .then((result) => {
       console.log("createPicture service process");
 
@@ -110,7 +92,7 @@ export default class PictureApi {
     const files = form.photos;
     delete form.photos;
 
-    PromiseApi.auth().uploads(`/proposals/${form._id}`, form, files, 'PUT')
+    PromiseApi.uploads(`/proposals/${form._id}`, form, files, 'PUT')
     .then((result) => {
       console.log("updatePicture service process");
 
@@ -127,61 +109,22 @@ export default class PictureApi {
     });
   }
 
-  // static update(form) {
-  //   const files = form.photos;
-  //   delete form.photos;
-  //
-  //   let promiseChain = null;
-  //
-  //   if (files.length > 0) {
-  //     promiseChain = PromiseApi.auth().uploads(`/proposals/${form._id}`, form, files, 'PUT');
-  //   } else {
-  //     promiseChain = PromiseApi.auth().put(`/proposals/${form._id}`, { proposalForm: form });
-  //   }
-  //
-  //   promiseChain
-  //     .then((res) => {
-  //       if (res.error) {
-  //         PicturesActions.error(res.error);
-  //       } else {
-  //         if (res.Picture.photoUrl === '') {
-  //           PromiseApi.download(`/public/proposals/${form._id}/image`)
-  //           .then((image) => {
-  //             res.Picture.images = [image];
-  //             PicturesActions.updateSuccess.defer(res.Picture);
-  //           });
-  //         } else {
-  //           res.Picture.images = [res.Picture.photoUrl];
-  //           PicturesActions.updateSuccess.defer(res.Picture);
-  //         }
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       PicturesActions.error(err);
-  //     });
-  // }
-
   static deletePicture(id) {
-    PromiseApi.auth().delete('/proposals/' + id)
+    PromiseApi.delete('/proposals/' + id)
     .then((result) => {
-      //console.log("deletePicture service process");
-
         if (result.error) {
-          //console.log("deletePicture service error");
           PictureActions.deletePictureError(result);
           return;
         }
         PictureActions.deletePictureSuccess(result);
     })
     .catch((err) => {
-      //console.log("requestVisit service catch ", err);
         PictureActions.deletePictureError(err);
     });
   }
 
   static togglePicture(id) {
-    //console.log("TOGGLE Start catch ", id);
-      PromiseApi.auth().put('/proposals/' + id + '/toggle')
+      PromiseApi.put('/proposals/' + id + '/toggle')
         .then((res) => {
           if (res.error) {
             PictureActions.togglePictureError(res.error);
@@ -190,31 +133,24 @@ export default class PictureApi {
           }
         })
         .catch((err) => {
-          //console.log("TOGGLE ERR catch ", err);
-
           PictureActions.togglePictureError(err);
         });
     }
 
 
   static requestVisit(params) {
-    //console.log("requestVisit", params[0], " && ", params[1]);
     PromiseApi.auth().post('/proposals/' + params[0] + '/visit', params[1])
     .then((result) => {
-      //console.log("requestVisit service process");
 
-        if (result.error) {
-          //console.log("requestVisit service error");
+      if (result.error) {
+        SearchActions.requestVisitError(result);
+        return;
+      }
 
-          SearchActions.requestVisitError(result);
-          return;
-        }
-
-        SearchActions.requestVisitSuccess(result);
+      SearchActions.requestVisitSuccess(result);
     })
     .catch((err) => {
-      //console.log("requestVisit service catch ", err);
-        SearchActions.requestVisitError(err);
+      SearchActions.requestVisitError(err);
     });
   }
 }
